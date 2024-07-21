7a24677176442aea5ed7860de6c244b8f8ae1596
Vanessa <116423200+Vanessa-BV@users.noreply.github.com>
2024-07-21 13:45:58 +0200

    Update ft_lstnew.c

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -34,7 +34,7 @@ t_list	*ft_lstnew(void *content)
 	t_list	*new;
 
 	new = (t_list *)malloc(sizeof(t_list) * 1);
-	if (new == 0)
+	if (new == NULL)
 		return (NULL);
 	new->content = content;
 	new->next = NULL;
